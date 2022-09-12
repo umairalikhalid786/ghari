69c346ea08cdc8af5737d5629e4a559cd4926b1f
Umair Ali Khalid <umair.ali.khalid@viaplaygroup.com>
2022-09-12 19:52:29 +0200

    Change 2

--- app/src/main/java/com/techlads/ghari/MyWatchFace.kt
+++ app/src/main/java/com/techlads/ghari/MyWatchFace.kt
@@ -64,6 +64,7 @@ class MyWatchFace : CanvasWatchFaceService() {
     override fun onCreateEngine(): Engine {
         return Engine()
         // Some basic changes
+        // Second change
     }
 
     private class EngineHandler(reference: MyWatchFace.Engine) : Handler(Looper.myLooper()!!) {
