82f1ca501e28cd1418fe16036a84ba31c0f3279f
Umair Ali Khalid <umair.ali.khalid@viaplaygroup.com>
2022-09-12 20:25:22 +0200

    Change 11 added in feature 3

--- app/src/main/java/com/techlads/ghari/MyWatchFace.kt
+++ app/src/main/java/com/techlads/ghari/MyWatchFace.kt
@@ -536,6 +536,7 @@ class MyWatchFace : CanvasWatchFaceService() {
     fun feature3() {
         // new feature added
         // 10th change feature 3
+        // 11th change feature 3
     }
 
     fun testFeature2() {
